14fd0153bcf29d60a404d1e6d337f4b5f20962ec
Helton Mori <heltonmori@gmail.com>
2024-09-18 13:10:23 -0300

    Aula 3 - Config firebase e vue

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -12,5 +9,4 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
-// Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig)
